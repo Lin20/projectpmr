8c448dd286ad66cd66927bf9a90c33166ce4b108
Lin <charliejennings2@hotmail.com>
2014-05-25 16:43:29 -0700

    Removed memory break so it compiles on non-windows platforms

--- src/Startup.cpp
+++ src/Startup.cpp
@@ -25,7 +25,7 @@ using namespace std;
 
 int main()
 {
-	_crtBreakAlloc = 5992;
+	//_crtBreakAlloc = 5992;
 	Engine::Initialize();
 
 	sf::RenderWindow window(sf::VideoMode(VIEWPORT_WIDTH * 16, VIEWPORT_HEIGHT * 16), "SFML works!");
